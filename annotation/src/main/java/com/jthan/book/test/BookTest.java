3402756f230c7f684a4af71ba4942fa323cc3e1b
Jthan <andismail@163.com>
2016-03-02 17:41:51 +0800

    注解小节,换思路,暂置

--- annotation/src/main/java/com/jthan/book/test/BookTest.java
+++ annotation/src/main/java/com/jthan/book/test/BookTest.java
@@ -6,13 +6,7 @@ package com.jthan.book.test;
 public class BookTest {
 
     public static void main(String[] args) {
-        System.out.println(Planet.MARS.surfaceGravity());
-
-        Operation[] ops = Operation.values();
-        double x = 2, y = 4;
-        for (Operation op : ops) {
-            System.out.printf("%f %s %f = %f%n", 2, op, 4, op.apply(x, y));
-        }
 
     }
+
 }
